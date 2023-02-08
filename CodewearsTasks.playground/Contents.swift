f6c72812fdaa73eaf26a84c92d3869c565afc922
Mustafa Bekirov <mustafos1999@icloud.com>
2023-02-08 20:58:26 +0100

    Work whith Codable

--- CodewearsTasks.playground/Contents.swift
+++ CodewearsTasks.playground/Contents.swift
@@ -4,6 +4,54 @@ func boolToWord(_ bool: Bool) -> String {
     bool ? "Yes" : "No"
 }
 
-func take(_ arr: [Int], _ n: Int) -> [Int] {
-    
+func reverse(_ str: String) -> String {
+    var reversedWord = String(str.reversed())
+    return reversedWord
 }
+
+func descendingOrder(of number: Int) -> Int {
+    var numStr = String(number)
+    var sortedStr = numStr.sorted(by: >)
+    for _ in sortedStr {
+        String(sortedStr)
+    }
+    let result = String(sortedStr)
+    let final = Int(result)
+    return final!
+}
+
+//func toLeetSpeak(_ s : String) -> String {
+    let alphabet = [
+        "A" : "@",
+        "B" : "8",
+        "C" : "(",
+        "D" : "D",
+        "E" : "3",
+        "F" : "F",
+        "G" : "6",
+        "H" : "#",
+        "I" : "!",
+        "J" : "J",
+        "K" : "K",
+        "L" : "1",
+        "M" : "M",
+        "N" : "N",
+        "O" : "0",
+        "P" : "P",
+        "Q" : "Q",
+        "R" : "R",
+        "S" : "$",
+        "T" : "7",
+        "U" : "U",
+        "V" : "V",
+        "W": "W",
+        "X" : "X",
+        "Y" : "Y",
+        "Z" : "2"
+    ]
+    
+let aString = String("LEET")
+for _ in alphabet {
+    aString. == alphabet.keys
+}
+
--- Networking/HTTPRequest/HTTPRequest/ViewController.swift
+++ Networking/HTTPRequest/HTTPRequest/ViewController.swift
@@ -7,16 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
-    let sessionConfiguration = URLSessionConfiguration.default
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataSource = [Post]()
+    
+    let networkManager = NetworkManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        URLSession(configuration: sessionConfiguration)
+        tableView.dataSource = self
+        
+        networkManager.obtainPosts { [weak self] (result) in
+            
+            switch result {
+                case .success(posts: let posts):
+                    
+                    self?.dataSource = posts
+                    
+                    DispatchQueue.main.async {
+                        self?.tableView.reloadData()
+                    }
+                case .failure(error: let error):
+                    print("Error: \(error.localizedDescription)")
+            }
+        }
     }
-
-
+    
+    //MARK: – UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "identifire", for: indexPath)
+        let post = dataSource[indexPath.row]
+        
+        cell.textLabel?.text = post.title
+        cell.detailTextLabel?.text = post.body
+        
+        return cell
+    }
 }
 
