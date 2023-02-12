4c36d2ad83c2ecbf4afcb40aa1f03c57951de5d0
Mustafa Bekirov <mustafos1999@icloud.com>
2023-02-13 00:09:03 +0100

    Add AbstractFactory

---
+++ ObjectOrientedProgramming/Patterns/AbstractFactory/AbstractFactory/Protocol/Sofa.swift
@@ -0,0 +1,13 @@
+//
+//  Sofa.swift
+//  AbstractFactory
+//
+//  Created by Mustafa Bekirov on 12.02.2023.
+//
+
+import Foundation
+
+protocol Sofa {
+    var name: String {get}
+    var type: String {get}
+}
