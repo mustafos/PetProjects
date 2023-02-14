91f86ddbb47e934ca0673bdf4c11e2ee9c60c4a0
Mustafa Bekirov <mustafos1999@icloud.com>
2023-02-14 22:09:17 +0100

    Create Adapter

---
+++ ObjectOrientedProgramming/Patterns/Adapter/Adapter/Adapter/TwitterAuthenticatorAdapter.swift
@@ -0,0 +1,8 @@
+//
+//  TwitterAuthenticatorAdapter.swift
+//  Adapter
+//
+//  Created by Mustafa Bekirov on 14.02.2023.
+//
+
+import Foundation
---
+++ ObjectOrientedProgramming/Patterns/Adapter/Adapter/Auth/TwitterAuthenticator.swift
@@ -0,0 +1,27 @@
+//
+//  TwitterAuthenticator.swift
+//  Adapter
+//
+//  Created by Mustafa Bekirov on 14.02.2023.
+//
+
+import Foundation
+
+public struct TwitterUser {
+    public var email: String
+    public var password: String
+    public var token: String
+}
+
+public class TwitterAuthenticator {
+    public func login(
+        email: String,
+        password: String,
+        completion: @escaping (TwitterUser?, Error?) -> Void) {
+            
+            let token = "special-token-value"
+            
+            let user = TwitterUser(email: email, password: password, token: token)
+            completion(user, nil)
+        }
+}
