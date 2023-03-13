d275058606c32abce265b76a55bd958405bfd6a0
Mustafa Bekirov <mustafos1999@icloud.com>
2023-03-13 16:41:56 +0100

    Add Dijkstra's algo

--- Algorithmus/Algorithmus.py
+++ Algorithmus/Algorithmus.py
@@ -258,6 +258,18 @@ def bf_search(name):
                 searched.append(person)
     return False
 
+# Dijkstra's algo
+processed = []
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
 ###Dynamic programming__________________________________________
 #Fibonacci sequence number
 def fib(n):
