5d1cdbc7df218ee17c6bdbd2be87032dfded37a0
Mustafa Bekirov <mustafos1999@icloud.com>
2023-04-16 10:35:25 +0200

    feat: add UI setting up and alamofire skeleton view

---
+++ Networking/WeatherAPI/WeatherApp/WeatherViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Mustafa Bekirov on 15.04.2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func addLocationButtonTapped(_ sender: Any) {
+    }
+
+    @IBAction func locationButtonTapped(_ sender: Any) {
+    }
+}
+
