3a2d709fb7277bfcb216a9e90e2ecac06cfef1b6
Mustafa Bekirov <mustafos1999@icloud.com>
2023-04-17 19:04:44 +0200

    fix: tap gestures

--- Networking/WeatherAPI/WeatherApp/Controller/AddCityViewController.swift
+++ Networking/WeatherAPI/WeatherApp/Controller/AddCityViewController.swift
@@ -9,13 +9,39 @@ import UIKit
 
 class AddCityViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
-        
+        setupGestures()
     }
     
     private func setupViews() {
         view.backgroundColor = UIColor(white: 0.3, alpha: 0.4)
+        cityTextField.becomeFirstResponder()
+    }
+    
+    private func setupGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissViewController))
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func dismissViewController() {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func searchButtonTapped(_ sender: Any) {
+    }
+}
+
+extension AddCityViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return touch.view == self.view
     }
 }
