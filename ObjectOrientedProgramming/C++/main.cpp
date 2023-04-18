efdb580ec811d89b786c43965f95bf80de59355b
Mustafa Bekirov <mustafos1999@icloud.com>
2023-04-18 12:42:00 +0200

    add polymorphism example in C++

--- ObjectOrientedProgramming/C++/main.cpp
+++ ObjectOrientedProgramming/C++/main.cpp
@@ -351,4 +351,197 @@ int main() {
   std::cout << fixed_multiply<int,3>(10) << '\n';
 }
 
-//________________________________________________________________________
+//___Polymorphism________________________________________________________
+// pointers to base class
+using namespace std;
+
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+};
+
+class Rectangle: public Polygon {
+  public:
+    int area()
+      { return width*height; }
+};
+
+class Triangle: public Polygon {
+  public:
+    int area()
+      { return width*height/2; }
+};
+
+int main () {
+  Rectangle rect;
+  Triangle trgl;
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  cout << rect.area() << '\n';
+  cout << trgl.area() << '\n';
+  return 0;
+}
+
+// virtual members
+using namespace std;
+
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+    virtual int area ()
+      { return 0; }
+};
+
+class Rectangle: public Polygon {
+  public:
+    int area ()
+      { return width * height; }
+};
+
+class Triangle: public Polygon {
+  public:
+    int area ()
+      { return (width * height / 2); }
+};
+
+int main () {
+  Rectangle rect;
+  Triangle trgl;
+  Polygon poly;
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  Polygon * ppoly3 = &poly;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  ppoly3->set_values (4,5);
+  cout << ppoly1->area() << '\n';
+  cout << ppoly2->area() << '\n';
+  cout << ppoly3->area() << '\n';
+  return 0;
+}
+
+// abstract class CPolygon
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+    virtual int area () =0;
+};
+
+// abstract base class
+using namespace std;
+
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+    virtual int area (void) =0;
+};
+
+class Rectangle: public Polygon {
+  public:
+    int area (void)
+      { return (width * height); }
+};
+
+class Triangle: public Polygon {
+  public:
+    int area (void)
+      { return (width * height / 2); }
+};
+
+int main () {
+  Rectangle rect;
+  Triangle trgl;
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  cout << ppoly1->area() << '\n';
+  cout << ppoly2->area() << '\n';
+  return 0;
+}
+
+// pure virtual members can be called
+// from the abstract base class
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+    virtual int area() =0;
+    void printarea()
+      { cout << this->area() << '\n'; }
+};
+
+class Rectangle: public Polygon {
+  public:
+    int area (void)
+      { return (width * height); }
+};
+
+class Triangle: public Polygon {
+  public:
+    int area (void)
+      { return (width * height / 2); }
+};
+
+int main () {
+  Rectangle rect;
+  Triangle trgl;
+  Polygon * ppoly1 = &rect;
+  Polygon * ppoly2 = &trgl;
+  ppoly1->set_values (4,5);
+  ppoly2->set_values (4,5);
+  ppoly1->printarea();
+  ppoly2->printarea();
+  return 0;
+}
+
+// dynamic allocation and polymorphism
+class Polygon {
+  protected:
+    int width, height;
+  public:
+    Polygon (int a, int b) : width(a), height(b) {}
+    virtual int area (void) =0;
+    void printarea()
+      { cout << this->area() << '\n'; }
+};
+
+class Rectangle: public Polygon {
+  public:
+    Rectangle(int a,int b) : Polygon(a,b) {}
+    int area()
+      { return width*height; }
+};
+
+class Triangle: public Polygon {
+  public:
+    Triangle(int a,int b) : Polygon(a,b) {}
+    int area()
+      { return width*height/2; }
+};
+
+int main () {
+  Polygon * ppoly1 = new Rectangle (4,5);
+  Polygon * ppoly2 = new Triangle (4,5);
+  ppoly1->printarea();
+  ppoly2->printarea();
+  delete ppoly1;
+  delete ppoly2;
+  return 0;
+}
