91654c95a6af24cea26ed6c8355cab93f7eca525
Mustafa Bekirov <mustafos1999@icloud.com>
2023-04-19 20:08:15 +0200

    feat: add setup location manager

--- Networking/WeatherAPI/WeatherApp/Controller/WeatherViewController.swift
+++ Networking/WeatherAPI/WeatherApp/Controller/WeatherViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SkeletonView
+import CoreLocation
 
 protocol WeatherViewControllerDelegate: AnyObject {
     func didUpdateWeatherFromSearch(model: WeatherModel)
@@ -19,6 +20,12 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionLabel: UILabel!
     
     private let weatherManager = WeatherManager()
+    
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        return manager
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,6 +78,27 @@ class WeatherViewController: UIViewController {
     }
 
     @IBAction func locationButtonTapped(_ sender: Any) {
+        switch CLLocationManager.authorizationStatus() {
+            case .authorizedAlways, .authorizedWhenInUse:
+                locationManager.requestLocation()
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            default:
+                promptForLocationPermission()
+                
+        }
+    }
+    
+    private func promptForLocationPermission() {
+        let alertController = UIAlertController(title: "Requires Location Permission", message: "Would you like to enable location permisssion in Settings?", preferredStyle: .alert)
+        let enableAction = UIAlertAction(title: "Go to Settings", style: .default) { _ in
+            guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else { return }
+            UIApplication.shared.open(settingsURL)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(enableAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true)
     }
 }
 
@@ -80,5 +108,15 @@ extension WeatherViewController: WeatherViewControllerDelegate {
             guard let this = self else { return }
             this.updateView(with: model)
         })
+    }
+}
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
     }
 }
