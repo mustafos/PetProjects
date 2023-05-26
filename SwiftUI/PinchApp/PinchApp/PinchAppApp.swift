9a45a4364a6414d2c2ad5d5b57f42a0bc89c4837
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-26 11:46:29 +0200

    init PinchApp project

---
+++ SwiftUI/PinchApp/PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Mustafa Bekirov on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
