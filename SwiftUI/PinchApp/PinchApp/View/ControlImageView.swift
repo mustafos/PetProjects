5a977b6c40379024afc26b9b9de658a496dd39a7
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-27 18:40:21 +0200

    feat: control interface

---
+++ SwiftUI/PinchApp/PinchApp/View/ControlImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ControlImageView.swift
+//  PinchApp
+//
+//  Created by Mustafa Bekirov on 27.05.2023.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView()
+    }
+}
