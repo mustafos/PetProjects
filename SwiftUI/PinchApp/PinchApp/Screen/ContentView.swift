b3d359d358acb91bacc518d650c626bab29c47d8
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-28 13:13:51 +0200

    feat: page thumbnails

--- SwiftUI/PinchApp/PinchApp/Screen/ContentView.swift
+++ SwiftUI/PinchApp/PinchApp/Screen/ContentView.swift
@@ -8,6 +8,9 @@ struct ContentView: View {
     @State private var imageOffset: CGSize = .zero
     @State private var isDrawerOpen: Bool = false
     
+    let pages: [Page] = pagesData
+    @State private var pageIndex: Int = 1
+    
     // MARK: – FUNCTION
     func resetImageState() {
         return withAnimation(.spring()) {
@@ -16,13 +19,17 @@ struct ContentView: View {
         }
     }
     
+    func currentPage() -> String {
+        return pages[pageIndex - 1].imageName
+    }
+    
     // MARK: – CONTENT
     var body: some View {
         NavigationView {
             ZStack {
                 Color.clear
                 // MARK: – PAGE IMAGE
-                Image("magazine-front-cover")
+                Image(currentPage())
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
@@ -154,6 +161,20 @@ struct ContentView: View {
                         })
                     
                     // MARK: – THUMBNAILS
+                    ForEach(pages) { item in
+                        Image(item.thumbnailName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
+                            .cornerRadius(8)
+                            .shadow(radius: 4)
+                            .opacity(isDrawerOpen ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isDrawerOpen)
+                            .onTapGesture {
+                                isAnimating = true
+                                pageIndex = item.id
+                            }
+                    }
                     Spacer()
                 } //: DRAWER
                     .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
