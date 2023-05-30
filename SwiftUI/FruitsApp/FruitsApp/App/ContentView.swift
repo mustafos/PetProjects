2ef5ccfaab72ec4e63055c34936d5bc5e902c26b
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-30 12:41:53 +0200

    feat: list view retrieve data with ForEach loop

--- SwiftUI/FruitsApp/FruitsApp/App/ContentView.swift
+++ SwiftUI/FruitsApp/FruitsApp/App/ContentView.swift
@@ -1,19 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: – PROPERTIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: – BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                    FruitRowView(fruit: item)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
+        } //: NAVIGATION
     }
 }
 
+// MARK: – PREVIEW
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
