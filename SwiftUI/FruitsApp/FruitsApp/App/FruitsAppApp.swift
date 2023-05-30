608eeecefcd900e9a4386a26ae7b6f5e56d505e1
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-30 12:23:56 +0200

    feat: storege to onboarding view

--- SwiftUI/FruitsApp/FruitsApp/App/FruitsAppApp.swift
+++ SwiftUI/FruitsApp/FruitsApp/App/FruitsAppApp.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 @main
 struct FruitsAppApp: App {
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
     var body: some Scene {
         WindowGroup {
-            OnboardingView()
+            if isOnboarding {
+                OnboardingView()
+            } else {
+                ContentView()
+            }
         }
     }
 }
--- SwiftUI/FruitsApp/FruitsApp/View/StartButtonView.swift
+++ SwiftUI/FruitsApp/FruitsApp/View/StartButtonView.swift
@@ -2,11 +2,12 @@ import SwiftUI
 
 struct StartButtonView: View {
     // MARK: – PROPERTIES
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
     
     // MARK: – BODY
     var body: some View {
         Button {
-            print("Exit the onboarding")
+            isOnboarding = false
         } label: {
             HStack(spacing: 8) {
                 Text("Start")
