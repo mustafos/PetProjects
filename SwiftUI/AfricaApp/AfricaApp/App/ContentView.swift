c2eb977d81f0ab9534bfc471cf2c518a3907941b
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-31 18:14:45 +0200

    feat: dynamic list pawered by generics and JSON data

--- SwiftUI/AfricaApp/AfricaApp/App/ContentView.swift
+++ SwiftUI/AfricaApp/AfricaApp/App/ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: – PROPERTIES
+    let animals: [Animal] = Bundle.main.decode("animals.json")
     
     // MARK: – BODY
     var body: some View {
@@ -11,6 +12,10 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             } //: LIST
             .navigationBarTitle("Africa", displayMode: .large)
         } //: NAVIGATION
--- SwiftUI/AfricaApp/AfricaApp/Extension/CodableBundleExtension.swift
+++ SwiftUI/AfricaApp/AfricaApp/Extension/CodableBundleExtension.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [CoverImage] {
+    func decode<T: Codable>(_ file: String) -> T {
         // 1. Locate the json file
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle")
@@ -16,7 +16,7 @@ extension Bundle {
         let decoder = JSONDecoder()
         
         // 4. Create a property for the decoded data
-        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle")
         }
         
