2831261b500337f6c017821e898e4d0295a58540
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-31 19:21:34 +0200

    feat: detail view for each animal with dynamic data

--- SwiftUI/AfricaApp/AfricaApp/App/ContentView.swift
+++ SwiftUI/AfricaApp/AfricaApp/App/ContentView.swift
@@ -14,8 +14,10 @@ struct ContentView: View {
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                 
                 ForEach(animals) { animal in
-                    AnimalListItemView(animal: animal)
-                }
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalListItemView(animal: animal)
+                    } //: LINK
+                } //: LOOP
             } //: LIST
             .navigationBarTitle("Africa", displayMode: .large)
         } //: NAVIGATION
---
+++ SwiftUI/AfricaApp/AfricaApp/View/AnimalDetailView.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct AnimalDetailView: View {
+    
+    // MARK: – PROPERTIES
+    let animal: Animal
+    
+    // MARK: – BODY
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                // GALLERY
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                // FACTS
+                
+                // DESCRIPTION
+                
+                // MAP
+                
+                // LINK
+                
+            } //: VSTACK
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        } //: SCROLL
+    }
+}
+
+// MARK: – PREVIEW
+struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        NavigationView {
+            AnimalDetailView(animal: animals[0])
+        }
+        .previewDevice("iPhone 13 mini")
+    }
+}
