c2fdba252505a681ef5e98346d9439e1dec2ebcd
Mustafa Bekirov <mustafos1999@icloud.com>
2023-06-03 13:38:35 +0200

    feat: motion animation

--- SwiftUI/AfricaApp/AfricaApp/App/GalleryView.swift
+++ SwiftUI/AfricaApp/AfricaApp/App/GalleryView.swift
@@ -1,11 +1,20 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    // MARK: – PROPERTIES
+    
+    // MARK: – BODY
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            Text("Gallery")
+        } //: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
+// MARK: – PREVIEW
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
