7c7600bfccde429ae5aef6c1cc925e78e5259a38
Mustafa Bekirov <mustafos1999@icloud.com>
2023-06-03 19:48:08 +0200

    feat: copiright reserved

--- SwiftUI/AfricaApp/AfricaApp/App/ContentView.swift
+++ SwiftUI/AfricaApp/AfricaApp/App/ContentView.swift
@@ -47,6 +47,8 @@ struct ContentView: View {
                                 AnimalListItemView(animal: animal)
                             } //: LINK
                         } //: LOOP
+                        CreditsView()
+                            .modifier(CenterModifier())
                     } //: LIST
                 } else {
                     ScrollView(.vertical, showsIndicators: false) {
