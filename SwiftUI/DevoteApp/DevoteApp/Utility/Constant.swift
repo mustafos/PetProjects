06ae10ba103d0835053a69464c61dd7695e9e42b
Mustafa Bekirov <mustafos1999@icloud.com>
2023-06-05 16:17:30 +0200

    feat: cor data template

---
+++ SwiftUI/DevoteApp/DevoteApp/Utility/Constant.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+// MARK: – FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: – UI
+
+// MARK: – UX
--- SwiftUI/DevoteApp/DevoteApp/View/ContentView.swift
+++ SwiftUI/DevoteApp/DevoteApp/View/ContentView.swift
@@ -2,13 +2,46 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    // MARK: – PROPERTY
+    
+    // FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    
+    // MARK: – FUNCTIONS
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
 
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    // MARK: – BODY
     var body: some View {
         NavigationView {
             List {
@@ -34,45 +67,9 @@ struct ContentView: View {
             Text("Select an item")
         } //: NAVIGATION
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+// MARK: – PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
