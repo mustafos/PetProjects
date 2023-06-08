8ddf6389df0a2b3651f3216b8dff0d2d7cc4c8e8
Mustafa Bekirov <mustafos1999@icloud.com>
2023-06-08 13:23:03 +0200

    feat: individual task item view

---
+++ SwiftUI/DevoteApp/DevoteApp/Style/CheckBoxStyle.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct CheckboxStyle: ToggleStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    return HStack {
+      Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+        .foregroundColor(configuration.isOn ? .pink : .primary)
+        .font(.system(size: 30, weight: .semibold, design: .rounded))
+        .onTapGesture {
+          configuration.isOn.toggle()
+        }
+      
+      configuration.label
+    } //: HSTACK
+  }
+}
+
+struct CheckboxStyle_Previews: PreviewProvider {
+  static var previews: some View {
+    Toggle("Placeholder label", isOn: .constant(false))
+      .toggleStyle(CheckboxStyle())
+      .padding()
+      .previewLayout(.sizeThatFits)
+  }
+}
---
+++ SwiftUI/DevoteApp/DevoteApp/View/ListRowItemView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct ListRowItemView: View {
+  @Environment(\.managedObjectContext) var viewContext
+  @ObservedObject var item: Item
+  
+  var body: some View {
+    Toggle(isOn: $item.completion) {
+      Text(item.task ?? "")
+        .font(.system(.title2, design: .rounded))
+        .fontWeight(.heavy)
+        .foregroundColor(item.completion ? Color.pink : Color.primary)
+        .padding(.vertical, 12)
+        .animation(.default)
+    } //: TOGGLE
+    .toggleStyle(CheckboxStyle())
+    .onReceive(item.objectWillChange, perform: { _ in
+      if self.viewContext.hasChanges {
+        try? self.viewContext.save()
+      }
+    })
+  }
+}
