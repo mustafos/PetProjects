a2e59681e8cb2fc847028bce8317e9a6c64fce77
mustafos <mustafos1999@gmail.com>
2023-06-09 12:50:41 +0200

    init NOTES watchOS project

---
+++ SwiftUI/NotesWatchApp/NotesWatchApp Watch App/NotesWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesWatchAppApp.swift
+//  NotesWatchApp Watch App
+//
+//  Created by Mustafa Bekirov on 09.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
