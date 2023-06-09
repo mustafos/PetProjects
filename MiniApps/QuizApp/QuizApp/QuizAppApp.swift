e0f19aff3bef7e1181e62d5345cda63b05a1e5b4
mustafos <mustafos1999@gmail.com>
2023-06-09 12:23:59 +0200

    add new functionality for hard skills

---
+++ MiniApps/QuizApp/QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Mustafa Bekirov on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
