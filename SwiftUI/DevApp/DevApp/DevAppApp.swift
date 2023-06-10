789e213618a30ce0490a8ce6b3299ecae750774b
mustafos <mustafos1999@gmail.com>
2023-06-10 17:49:26 +0200

    init Developers App

---
+++ SwiftUI/DevApp/DevApp/DevAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevAppApp.swift
+//  DevApp
+//
+//  Created by Mustafa Bekirov on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
