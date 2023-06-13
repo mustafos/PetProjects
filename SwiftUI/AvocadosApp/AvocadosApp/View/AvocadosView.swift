17277db0da07ac660485b244bce47d0274c8b237
mustafos <mustafos1999@gmail.com>
2023-06-13 13:14:27 +0200

    feat: avocado home view

--- SwiftUI/AvocadosApp/AvocadosApp/View/AvocadosView.swift
+++ SwiftUI/AvocadosApp/AvocadosApp/View/AvocadosView.swift
@@ -1,8 +1,52 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    
+    // MARK: – PROPERTIES
+    
+    @State private var pulsateAnimation: Bool = false
+    
     var body: some View {
-        Text("Avocados")
+        VStack {
+            Spacer()
+            
+            Image("avocado")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 240, height: 240, alignment: .center)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+                .scaleEffect(self.pulsateAnimation ? 1 : 0.9)
+                .opacity(self.pulsateAnimation ? 1 : 0.9)
+                .animation(Animation.easeOut(duration: 1.5).repeatForever(autoreverses: true))
+            
+            VStack {
+                Text("Avocados".uppercased())
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+                Text("""
+            Creamy, green, and full of nutrients! Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+            """)
+                .lineLimit(nil)
+                .font(.system(.headline, design: .serif))
+                .foregroundColor(Color("ColorGreenLight"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .frame(maxWidth: 640, minHeight: 120)
+                .padding()
+            }
+            Spacer()
+        }
+        .background(
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        )
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            self.pulsateAnimation.toggle()
+        }
     }
 }
 
