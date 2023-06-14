303879b31d0ace423044321f7c8eb68352e65163
mustafos <mustafos1999@gmail.com>
2023-06-14 09:19:17 +0200

    feat: feat: design and present settings view

--- SwiftUI/AvocadosApp/AvocadosApp/Model/RipeningModel.swift
+++ SwiftUI/AvocadosApp/AvocadosApp/Model/RipeningModel.swift
@@ -1,8 +1,13 @@
-//
-//  RipeningModel.swift
-//  AvocadosApp
-//
-//  Created by Mustafa Bekirov on 14.06.2023.
-//
+import SwiftUI
 
-import Foundation
+// MARK: RIPENING MODEL
+
+struct Ripening: Identifiable {
+  var id = UUID()
+  var image: String
+  var stage: String
+  var title: String
+  var description: String
+  var ripeness: String
+  var instruction: String
+}
--- SwiftUI/AvocadosApp/AvocadosApp/View/RipeningStagesView.swift
+++ SwiftUI/AvocadosApp/AvocadosApp/View/RipeningStagesView.swift
@@ -1,13 +1,30 @@
 import SwiftUI
 
 struct RipeningStagesView: View {
+    // MARK: - PROPERTIES
+    
+    var ripeningStages: [Ripening] = ripeningData
+    
     var body: some View {
-        Text("Ripening Stages")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                HStack(alignment: .center, spacing: 25) {
+                    ForEach(ripeningStages) { item in
+                        RipeningView(ripening: item)
+                    }
+                }
+                .padding(.vertical)
+                .padding(.horizontal, 25)
+                Spacer()
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct RipeningStagesView_Previews: PreviewProvider {
     static var previews: some View {
-        RipeningStagesView()
+        RipeningStagesView(ripeningStages: ripeningData)
     }
 }
