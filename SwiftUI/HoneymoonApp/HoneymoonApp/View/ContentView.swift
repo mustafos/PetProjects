14d6c5f616b9cc4e44d018d0c1302aa44032f1ed
mustafos <mustafos1999@gmail.com>
2023-06-22 12:28:24 +0200

    feat: swiping motion

--- SwiftUI/HoneymoonApp/HoneymoonApp/View/ContentView.swift
+++ SwiftUI/HoneymoonApp/HoneymoonApp/View/ContentView.swift
@@ -6,6 +6,7 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
+    @GestureState private var dragState = DragState.inactive
     
     // MARK: – CARD VIEWS
     
@@ -25,10 +26,47 @@ struct ContentView: View {
         return index == 0
     }
     
+    // MARK: DRAG STATES
+    
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+                case .inactive, .pressing:
+                    return .zero
+                case .dragging(let translation):
+                    return translation
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+                case .dragging:
+                    return true
+                case .pressing, .inactive:
+                    return false
+            }
+        }
+        
+        var isPressing: Bool {
+            switch self {
+                case .pressing, .dragging:
+                    return true
+                case .inactive:
+                    return false
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
             // MARK: – HEADER
             HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default)
             
             Spacer()
             
@@ -37,6 +75,23 @@ struct ContentView: View {
                 ForEach(cardViews) { cardView in
                     cardView
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                        .offset(x: self.isTopCard(cardView: cardView) ? self.dragState.translation.width : 0, y: self.isTopCard(cardView: cardView) ? self.dragState.translation.height : 0)
+                        .scaleEffect(self.dragState.isDragging && self.isTopCard(cardView: cardView) ? 0.85 : 1.0)
+                        .rotationEffect(Angle(degrees: self.isTopCard(cardView: cardView) ? Double(self.dragState.translation.width / 12) : 0))
+                        .animation(.interpolatingSpring(stiffness: 120, damping: 120))
+                        .gesture(LongPressGesture(minimumDuration: 0.01)
+                            .sequenced(before: DragGesture())
+                            .updating(self.$dragState, body: { (value, state, transaction) in
+                                switch value {
+                                    case .first(true):
+                                        state = .pressing
+                                    case .second(true, let drag):
+                                        state = .dragging(translation: drag?.translation ?? .zero)
+                                    default:
+                                        break
+                                }
+                            })
+                        )
                 }
             }
             .padding(.horizontal)
@@ -45,6 +100,8 @@ struct ContentView: View {
             
             // MARK: – FOOTER
             FooterView(showBookingAlert: $showAlert)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default)
         }
         .alert(isPresented: $showAlert) {
             Alert(
