4d77ea5a3b6b2b4fd5576c14a913d10e275adc9f
mustafos <mustafos1999@gmail.com>
2023-06-23 18:49:46 +0200

    feat: display the symbols during the swiping

--- SwiftUI/HoneymoonApp/HoneymoonApp/View/ContentView.swift
+++ SwiftUI/HoneymoonApp/HoneymoonApp/View/ContentView.swift
@@ -7,6 +7,7 @@ struct ContentView: View {
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
     @GestureState private var dragState = DragState.inactive
+    private let dragAreaThreshold: CGFloat = 65.0
     
     // MARK: – CARD VIEWS
     
@@ -75,6 +76,21 @@ struct ContentView: View {
                 ForEach(cardViews) { cardView in
                     cardView
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                        .overlay(content: {
+                            ZStack {
+                                // X-MARK SYMBOL
+                                Image(systemName: "x.circle")
+                                    .modifier(SymbolModifier())
+                                    .opacity(self.dragState.translation.width < -self.dragAreaThreshold
+                                             && self.isTopCard(cardView: cardView) ? 1.0 : 0.0)
+                                
+                                // HEART SYMBOL
+                                Image(systemName: "heart.circle")
+                                    .modifier(SymbolModifier())
+                                    .opacity(self.dragState.translation.width > self.dragAreaThreshold
+                                             && self.isTopCard(cardView: cardView) ? 1.0 : 0.0)
+                            }
+                        })
                         .offset(x: self.isTopCard(cardView: cardView) ? self.dragState.translation.width : 0, y: self.isTopCard(cardView: cardView) ? self.dragState.translation.height : 0)
                         .scaleEffect(self.dragState.isDragging && self.isTopCard(cardView: cardView) ? 0.85 : 1.0)
                         .rotationEffect(Angle(degrees: self.isTopCard(cardView: cardView) ? Double(self.dragState.translation.width / 12) : 0))
