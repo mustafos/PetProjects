1d23d7ba19d2d52fe1087294c42facf9bee5dd95
mustafos <mustafos1999@gmail.com>
2023-06-27 22:47:08 +0200

    SwiftUI basic

---
+++ SwiftUI/ProUISwift/ProUISwift/ProUISwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProUISwiftApp.swift
+//  ProUISwift
+//
+//  Created by Mustafa Bekirov on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProUISwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
