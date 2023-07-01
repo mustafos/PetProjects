ae8de553cf4e1b72962805bf9191b00c4ca594d6
mustafos <mustafos1999@gmail.com>
2023-07-01 22:09:21 +0200

    feat: result view

---
+++ MiniApps/TipCalculator/TipCalculator/Extension/UIView+Extension.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension UIView {
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+}
--- MiniApps/TipCalculator/TipCalculator/View/ResultView.swift
+++ MiniApps/TipCalculator/TipCalculator/View/ResultView.swift
@@ -1,6 +1,56 @@
 import UIKit
+import SnapKit
 
 class ResultView: UIView {
+    
+    private let headerLabel: UILabel = {
+        LabelFactory.build(text: "Total p/person", font: ThemeFont.demiBold(ofSize: 18))
+    }()
+    
+    private let amountPerPersonLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        let text = NSMutableAttributedString(
+            string: "$0",
+            attributes: [
+                .font: ThemeFont.bold(ofSize: 40)
+            ])
+        text.addAttributes([
+            .font: ThemeFont.bold(ofSize: 24)
+        ], range: NSMakeRange(0, 1))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let horizontalLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = ThemeColot.separator
+        return view
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            headerLabel,
+            amountPerPersonLabel,
+            horizontalLineView,
+            buildSpacerView(height: 0),
+            hStackView
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            AmountView(),
+            UIView(),
+            AmountView()
+        ])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
     
     init() {
         super.init(frame: .zero)
@@ -12,6 +62,43 @@ class ResultView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .gray
+        backgroundColor = .white
+        addSubview(vStackView)
+        vStackView.snp.makeConstraints { make in
+            make.top.equalTo(snp.top).offset(24)
+            make.leading.equalTo(snp.leading).offset(24)
+            make.trailing.equalTo(snp.trailing).offset(-24)
+            make.bottom.equalTo(snp.bottom).offset(-24)
+        }
+        horizontalLineView.snp.makeConstraints { make in
+            make.height.equalTo(2)
+        }
+        
+        addShadow(
+            offset: CGSize(width: 0, height: 3),
+            color: .black,
+            radius: 12.0,
+            opacity: 0.1)
+    }
+    
+    private func buildSpacerView(height: CGFloat) -> UIView {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: height).isActive = true
+        return view
+    }
+}
+
+class AmountView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() -> Void {
+        backgroundColor = .red
     }
 }
