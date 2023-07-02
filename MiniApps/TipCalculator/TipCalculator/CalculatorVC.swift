6a8e65780b00fb74efbaacda629dfdaa42db8029
mustafos <mustafos1999@gmail.com>
2023-07-02 16:44:59 +0200

    feat: view model

--- MiniApps/TipCalculator/TipCalculator/CalculatorVC.swift
+++ MiniApps/TipCalculator/TipCalculator/CalculatorVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import Combine
 
 class CalculatorVC: UIViewController {
     
@@ -29,10 +30,27 @@ class CalculatorVC: UIViewController {
         stackView.spacing = 36
         return stackView
     }()
+    
+    private let vm = CalculatorVM()
+    private var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
+        bind()
+    }
+    
+    private func bind() {
+        let input = CalculatorVM.Input(
+            billPublisher: Just(10).eraseToAnyPublisher(),
+            tipPublisher: Just(.tenPercent).eraseToAnyPublisher(),
+            splitPublisher: Just(5).eraseToAnyPublisher())
+        
+        let output = vm.transform(input: input)
+        
+        output.updateViewPublisher.sink { result in
+            print(">>>> \(result)")
+        }.store(in: &cancellables)
     }
     
     private func layout() {
---
+++ MiniApps/TipCalculator/TipCalculator/ViewModel/CalculatorVM.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Combine
+
+class CalculatorVM {
+    struct Input {
+        let billPublisher: AnyPublisher<Double, Never>
+        let tipPublisher: AnyPublisher<Tip, Never>
+        let splitPublisher: AnyPublisher<Int, Never>
+    }
+    
+    struct Output {
+        let updateViewPublisher: AnyPublisher<Result, Never>
+    }
+    
+    func transform(input: Input) -> Output {
+        let result = Result(
+            amountPerPerson: 500,
+            totalBill: 1000,
+            totalTip: 50.0)
+        
+        return Output(updateViewPublisher: Just(result).eraseToAnyPublisher())
+    }
+}
