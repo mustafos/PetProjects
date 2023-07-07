a599be514e457e692c7fa31e01c116672b91d716
mustafos <mustafos1999@gmail.com>
2023-07-07 15:20:15 +0200

    test: result with custom tip for persons

--- MiniApps/TipCalculator/TipCalculatorTests/TipCalculatorTests.swift
+++ MiniApps/TipCalculator/TipCalculatorTests/TipCalculatorTests.swift
@@ -37,6 +37,45 @@ final class TipCalculatorTests: XCTestCase {
         }.store(in: &cancellables)
     }
     
+    func testResultWithoutTipFor2Person() {
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 50)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWith10PercentTipFor3Person() {
+        let bill: Double = 100.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 55)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithCustomTipFor4Person() {
+        let bill: Double = 200.0
+        let tip: Tip = .custom(value: 201)
+        let split: Int = 4
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        let output = sut.transform(input: input)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 100.25)
+            XCTAssertEqual(result.totalBill, 401)
+            XCTAssertEqual(result.totalTip, 201)
+        }.store(in: &cancellables)
+    }
+    
     private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorVM.Input {
         return .init(
             billPublisher: Just(bill).eraseToAnyPublisher(),
