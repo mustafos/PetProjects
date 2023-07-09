482e60fcbdb4f28d4c2d03b2f1254a2b344dd3bf
mustafos <mustafos1999@gmail.com>
2023-07-09 16:50:17 +0200

    test: regular & custom tip flow

--- MiniApps/TipCalculator/TipCalculator/Service/AudioPlayerService.swift
+++ MiniApps/TipCalculator/TipCalculator/Service/AudioPlayerService.swift
@@ -2,21 +2,21 @@ import Foundation
 import AVFoundation
 
 protocol AudioPlayerService {
-  func playSound()
+    func playSound()
 }
 
 final class DefaultAudioPlayer: AudioPlayerService {
-  
-  private var player: AVAudioPlayer?
-  
-  func playSound() {
-    let path = Bundle.main.path(forResource: "click", ofType: "m4a")!
-    let url = URL(fileURLWithPath: path)
-    do {
-      player = try AVAudioPlayer(contentsOf: url)
-      player?.play()
-    } catch(let error) {
-      print(error.localizedDescription)
+    
+    private var player: AVAudioPlayer?
+    
+    func playSound() {
+        let path = Bundle.main.path(forResource: "click", ofType: "m4a")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch(let error) {
+            print(error.localizedDescription)
+        }
     }
-  }
 }
--- MiniApps/TipCalculator/TipCalculatorTests/TipCalculatorTests.swift
+++ MiniApps/TipCalculator/TipCalculatorTests/TipCalculatorTests.swift
@@ -26,12 +26,15 @@ final class TipCalculatorTests: XCTestCase {
         logoViewTapSubject = nil
     }
     
-    func testResultWithoutTipForPerson() {
+    func testResultWithoutTipFor1Person() {
         // given
         let bill: Double = 100.0
         let tip: Tip = .none
         let split: Int = 1
-        let input = buildInput(bill: bill, tip: tip, split: split)
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
         // when
         let output = sut.transform(input: input)
         // then
@@ -43,11 +46,17 @@ final class TipCalculatorTests: XCTestCase {
     }
     
     func testResultWithoutTipFor2Person() {
+        // given
         let bill: Double = 100.0
         let tip: Tip = .none
         let split: Int = 2
-        let input = buildInput(bill: bill, tip: tip, split: split)
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        // when
         let output = sut.transform(input: input)
+        // then
         output.updateViewPublisher.sink { result in
             XCTAssertEqual(result.amountPerPerson, 50)
             XCTAssertEqual(result.totalBill, 100)
@@ -55,12 +64,18 @@ final class TipCalculatorTests: XCTestCase {
         }.store(in: &cancellables)
     }
     
-    func testResultWith10PercentTipFor3Person() {
+    func testResultWith10PercentTipFor2Person() {
+        // given
         let bill: Double = 100.0
         let tip: Tip = .tenPercent
         let split: Int = 2
-        let input = buildInput(bill: bill, tip: tip, split: split)
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        // when
         let output = sut.transform(input: input)
+        // then
         output.updateViewPublisher.sink { result in
             XCTAssertEqual(result.amountPerPerson, 55)
             XCTAssertEqual(result.totalBill, 110)
@@ -69,11 +84,17 @@ final class TipCalculatorTests: XCTestCase {
     }
     
     func testResultWithCustomTipFor4Person() {
+        // given
         let bill: Double = 200.0
         let tip: Tip = .custom(value: 201)
         let split: Int = 4
-        let input = buildInput(bill: bill, tip: tip, split: split)
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split)
+        // when
         let output = sut.transform(input: input)
+        // then
         output.updateViewPublisher.sink { result in
             XCTAssertEqual(result.amountPerPerson, 100.25)
             XCTAssertEqual(result.totalBill, 401)
@@ -82,13 +103,16 @@ final class TipCalculatorTests: XCTestCase {
     }
     
     func testSoundPlayedAndCalculatorResetOnLogoViewTap() {
+        // given
         let input = buildInput(bill: 100, tip: .tenPercent, split: 2)
         let output = sut.transform(input: input)
         let expectation1 = XCTestExpectation(description: "reset calculator called")
         let expectation2 = audioPlayerService.expectation
+        // then
         output.resetCalculatorPublisher.sink { _ in
             expectation1.fulfill()
         }.store(in: &cancellables)
+        // when
         logoViewTapSubject.send()
         wait(for: [expectation1, expectation2], timeout: 1.0)
     }
