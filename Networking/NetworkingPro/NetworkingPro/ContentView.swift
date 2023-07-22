db46a591cda38334725d7dbe3778f78da3dcf30e
mustafos <mustafos1999@gmail.com>
2023-07-22 20:30:29 +0200

    feat: network service with async

--- Networking/NetworkingPro/NetworkingPro/ContentView.swift
+++ Networking/NetworkingPro/NetworkingPro/ContentView.swift
@@ -1,21 +1,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var user: UserResults.User?
     var body: some View {
-        VStack {
+        HStack {
             Image(systemName: "globe")
+                .resizable()
+                .frame(width: 80, height: 80)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack(alignment: .leading) {
+                Text("\(user?.name.first ?? "Name") \((user?.name.last ?? "Last name"))")
+                    .font(.title2)
+                    .bold()
+                Text("\(user?.email ?? "email")")
+                Text("Sex: \(user?.gender ?? "No")")
+            }
         }
         .onAppear {
-            NetworkServiceWithCompletions.shared.fetchData { result in
-                switch result {
-                    case .success(let usersData):
-                        print("Data: \(usersData.results[0].email)")
-                    case .failure(let failure):
-                        print(failure.localizedDescription)
-                }
+//            NetworkServiceWithCompletions.shared.fetchData { result in
+//                switch result {
+//                    case .success(let usersData):
+//                        let user = usersData.results[0]
+//                        self.user = user
+//                    case .failure(let failure):
+//                        print(failure.localizedDescription)
+//                }
+//            }
+            Task {
+                let result = try await NetworkServiceWithAsync.shared.fetchData()
+                self.user = result.results[0]
             }
         }
         .padding()
