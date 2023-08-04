c691d0a0627bdf6287e6d951244ac93a58442176
mustafos <mustafos1999@gmail.com>
2023-08-04 19:35:20 +0200

    feat: view logic with chat bubble ui

---
+++ SwiftUI/TwitterClone/TwitterClone/View/Messages/ChatBubble.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct ChatBubble: Shape {
+    
+    var isFromCurrentUser: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight, isFromCurrentUser ? .bottomLeft : .bottomRight], cornerRadii: CGSize(width: 16, height: 16))
+        
+        return Path(path.cgPath)
+    }
+}
+
+struct ChatBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatBubble(isFromCurrentUser: false)
+    }
+}
--- SwiftUI/TwitterClone/TwitterClone/View/Messages/ChatView.swift
+++ SwiftUI/TwitterClone/TwitterClone/View/Messages/ChatView.swift
@@ -9,7 +9,15 @@ struct ChatView: View {
             ScrollView {
                 VStack(alignment: .leading, spacing: 12) {
                     ForEach(0..<15) { _ in
-                        Text("Chat bubble")
+                        HStack {
+                            Spacer()
+                            Text("Test message text")
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(ChatBubble(isFromCurrentUser: true))
+                                .foregroundColor(.white)
+                                .padding(.horizontal)
+                        }
                     }
                 }
             }
