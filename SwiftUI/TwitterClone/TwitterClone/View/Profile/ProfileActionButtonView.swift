016fc5b809c1ea50c4b89eb0630b4086046e6d7f
mustafos <mustafos1999@gmail.com>
2023-08-08 20:11:34 +0200

    fix: profile action button view

--- SwiftUI/TwitterClone/TwitterClone/View/Profile/ProfileActionButtonView.swift
+++ SwiftUI/TwitterClone/TwitterClone/View/Profile/ProfileActionButtonView.swift
@@ -1,13 +1,46 @@
 import SwiftUI
 
 struct ProfileActionButtonView: View {
+    let isCurrentUser: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isCurrentUser {
+            Button {
+                print("edit profile")
+            } label: {
+                Text("Edit Profile")
+                    .frame(width: 360, height: 40)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+            }
+            .cornerRadius(20)
+        } else {
+            HStack {
+                Button {
+                    print("follow")
+                } label: {
+                    Text("Follow")
+                        .frame(width: 180, height: 40)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                }
+                .cornerRadius(20)
+                
+                Button {
+                    print("message")
+                } label: {
+                    Text("Message")
+                        .frame(width: 180, height: 40)
+                        .background(Color.purple)
+                        .foregroundColor(.white)
+                }
+                .cornerRadius(20)
+            }
+        }
     }
 }
 
 struct ProfileActionButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileActionButtonView()
+        ProfileActionButtonView(isCurrentUser: false)
     }
 }
--- SwiftUI/TwitterClone/TwitterClone/View/Profile/ProfileHeaderView.swift
+++ SwiftUI/TwitterClone/TwitterClone/View/Profile/ProfileHeaderView.swift
@@ -9,7 +9,7 @@ struct ProfileHeaderView: View {
                 .clipped()
                 .frame(width: 120, height: 120)
                 .cornerRadius(120 / 2)
-                .shadow(color: .black, radius: 10, x: 0.0, y: 0.0)
+                .shadow(color: .black, radius: 6, x: 0.0, y: 0.0)
             
             Text("Tutankhamon")
                 .font(.system(size: 16, weight: .semibold))
@@ -42,6 +42,8 @@ struct ProfileHeaderView: View {
                 }
             }
             .padding()
+            
+            ProfileActionButtonView(isCurrentUser: false)
             
             Spacer()
         }
