e38ee59e4a2c9fb91ef7cc1f4f0914be7d18e5a1
mustafos <mustafos1999@gmail.com>
2023-08-09 12:37:40 +0200

    feat: filter button view

--- SwiftUI/TwitterClone/TwitterClone/View/Profile/FilterButtonView.swift
+++ SwiftUI/TwitterClone/TwitterClone/View/Profile/FilterButtonView.swift
@@ -1,13 +1,59 @@
 import SwiftUI
 
+enum TweetFilterOptions: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+    
+    var title: String {
+        switch self {
+            case .tweets:
+                return "Tweets"
+            case .replies:
+                return "Tweets & Replies"
+            case .likes:
+                return "Likes"
+        }
+    }
+}
+
 struct FilterButtonView: View {
+    
+    @Binding var selectedOption: TweetFilterOptions
+    
+    private let underlineWidth = UIScreen.main.bounds.width / CGFloat(TweetFilterOptions.allCases.count)
+    
+    private var padding: CGFloat {
+        let rawValue = CGFloat(selectedOption.rawValue)
+        let count = CGFloat(TweetFilterOptions.allCases.count)
+        return ((UIScreen.main.bounds.width / count) * rawValue) + 16
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                ForEach(TweetFilterOptions.allCases, id: \.self) { option in
+                    Button {
+                        self.selectedOption = option
+                    } label: {
+                        Text(option.title)
+                            .frame(width: underlineWidth - 8)
+                    }
+
+                }
+            }
+            
+            Rectangle()
+                .frame(width: underlineWidth - 32, height: 3, alignment: .center)
+                .foregroundColor(.blue)
+                .padding(.leading, padding)
+                .animation(.spring())
+        }
     }
 }
 
 struct FilterButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        FilterButtonView()
+        FilterButtonView(selectedOption: .constant(.replies))
     }
 }
--- SwiftUI/TwitterClone/TwitterClone/View/Profile/UserProfileView.swift
+++ SwiftUI/TwitterClone/TwitterClone/View/Profile/UserProfileView.swift
@@ -1,11 +1,28 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    let user: User
+    @ObservedObject var viewModel: ProfileViewModel
+    @State private var selectedFilter: TweetFilterOptions = .tweets
+    
+    init(user: User, selectedFilter: TweetFilterOptions) {
+        self.user = user
+        self.selectedFilter = selectedFilter
+    }
+    
     var body: some View {
         ScrollView {
             VStack {
                 ProfileHeaderView()
                     .padding()
+                
+                FilterButtonView(selectedOption: $selectedFilter)
+                    .padding()
+                
+                ForEach(0..<9) { tweet in
+                    TweetCell()
+                }
+                .padding()
             }
             .navigationTitle("pharaon")
         }
