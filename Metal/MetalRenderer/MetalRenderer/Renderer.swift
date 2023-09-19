a4ae541db28715a43cd126a4ff3b8e14bf14e118
mustafos <mustafos1999@gmail.com>
2023-09-19 21:02:15 +0200

    feat: projection with depth

--- Metal/MetalRenderer/MetalRenderer/Renderer.swift
+++ Metal/MetalRenderer/MetalRenderer/Renderer.swift
@@ -17,8 +17,10 @@ struct Vertex {
 class Renderer: NSObject {
     static var device: MTLDevice!
     let commandQueue: MTLCommandQueue
+    
     static var library: MTLLibrary!
     let pipelineState: MTLRenderPipelineState
+    let depthStencilState: MTLDepthStencilState
     
     let train: Model
     let tree: Model
@@ -32,30 +34,40 @@ class Renderer: NSObject {
         self.commandQueue = commandQueue
         Renderer.library = device.makeDefaultLibrary()!
         pipelineState = Renderer.createPipelineState()
+        depthStencilState = Renderer.createDepthState()
+        
+        view.depthStencilPixelFormat = .depth32Float
         
         train = Model(name: "train")
         train.transform.position = [0.4, 0, 0]
         train.transform.scale = 0.5
         
         tree = Model(name: "treefir")
-        tree.transform.position = [-0.6, 0, 0.3]
+        tree.transform.position = [-1, 0, 0.3]
         tree.transform.scale = 0.5
         
         super.init()
     }
     
+    static func createDepthState() -> MTLDepthStencilState {
+        let depthDescriptor = MTLDepthStencilDescriptor()
+        depthDescriptor.depthCompareFunction = .less
+        depthDescriptor.isDepthWriteEnabled = true
+        return Renderer.device.makeDepthStencilState(descriptor: depthDescriptor)!
+    }
+    
     static func createPipelineState() -> MTLRenderPipelineState {
-        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
-        
-        // pipeline state properties
-        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
         let vertexFunction = Renderer.library.makeFunction(name: "vertex_main")
         let fragmentFunction = Renderer.library.makeFunction(name: "fragment_main")
-        pipelineStateDescriptor.vertexFunction = vertexFunction
-        pipelineStateDescriptor.fragmentFunction = fragmentFunction
-        pipelineStateDescriptor.vertexDescriptor = MTLVertexDescriptor.defaultVertexDescriptor()
         
-        return try! Renderer.device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        pipelineDescriptor.vertexDescriptor = MTLVertexDescriptor.defaultVertexDescriptor()
+        pipelineDescriptor.depthAttachmentPixelFormat = .depth32Float
+        
+        return try! Renderer.device.makeRenderPipelineState(descriptor: pipelineDescriptor)
     }
 }
 
@@ -65,27 +77,35 @@ extension Renderer: MTKViewDelegate {
     
     func draw(in view: MTKView) {
         guard let commandBuffer = commandQueue.makeCommandBuffer(),
-                let drawable = view.currentDrawable,
-              let descriptor = view.currentRenderPassDescriptor,
-              let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor)  else {
+              let drawable = view.currentDrawable,
+              let descriptor = view.currentRenderPassDescriptor else {
             return
         }
         
+        let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor)!
         commandEncoder.setRenderPipelineState(pipelineState)
+        commandEncoder.setDepthStencilState(depthStencilState)
+        
+        let projectionMetrix = float4x4(projectionFov: radians(fromDegrees: 65),
+                                        near: 0.1, far: 100,
+                                        aspect: Float(view.bounds.width / view.bounds.height))
         
         var viewTransform = Transform()
         viewTransform.position.y = 1.0
+        viewTransform.position.z = -2.0
         
-        var viewMetrix = viewTransform.matrix.inverse
+        var viewMetrix = projectionMetrix * viewTransform.matrix.inverse
         commandEncoder.setVertexBytes(&viewMetrix,
                                       length: MemoryLayout<float4x4>.stride,
                                       index: 22)
         
         let models = [tree, train]
         for model in models {
-        var modelMatrix = model.transform.matrix
-        commandEncoder.setVertexBytes(&modelMatrix, length: MemoryLayout<float4x4>.stride, index: 21)
-        
+            var modelMatrix = model.transform.matrix
+            commandEncoder.setVertexBytes(&modelMatrix,
+                                          length: MemoryLayout<float4x4>.stride,
+                                          index: 21)
+            
             for mtkMesh in model.mtkMeshes {
                 for vertexBuffer in mtkMesh.vertexBuffers {
                     commandEncoder.setVertexBuffer(vertexBuffer.buffer, offset: 0, index: 0)
