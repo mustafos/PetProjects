19f833489017f63b967835272545ca077c09df2c
Mustafa Bekirov <mustafos1999@gmail.com>
2023-12-26 09:16:10 +0100

    feat: custom tab bar

---
+++ SwiftUI/Fundamentals/WeatherA/WeatherA/TabBar/AppTabBar2.swift
@@ -0,0 +1,18 @@
+//
+//  AppTabBar2.swift
+//  WeatherA
+//
+//  Created by Mustafa Bekirov on 25.12.2023.
+//
+
+import SwiftUI
+
+struct AppTabBar2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppTabBar2()
+}
