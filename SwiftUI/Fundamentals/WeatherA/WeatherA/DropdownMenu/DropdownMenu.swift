4148f42fafd2b7d3d0505ace0627a35fa41ae67d
Mustafa Bekirov <mustafos1999@gmail.com>
2023-12-28 09:42:42 +0100

    feat: dropdown menu

--- SwiftUI/Fundamentals/WeatherA/WeatherA/DropdownMenu/DropdownMenu.swift
+++ SwiftUI/Fundamentals/WeatherA/WeatherA/DropdownMenu/DropdownMenu.swift
@@ -2,14 +2,30 @@
 //  DropdownMenu.swift
 //  WeatherA
 //
-//  Created by Mustafa Bekirov on 26.12.2023.
+//  Created by Mustafa Bekirov on 27.12.2023.
 //
 
 import SwiftUI
 
+struct DropMenu: Identifiable {
+    var id = UUID()
+    var title: String
+}
+let drop = [
+    DropMenu(title: "Item 1"),
+    DropMenu(title: "Item 2"),
+    DropMenu(title: "Item 3"),
+    DropMenu(title: "Item 4"),
+    DropMenu(title: "Item 5"),
+    DropMenu(title: "Item 6")
+]
+
 struct DropdownMenu: View {
+    @State var show: Bool = true
+    @State var name: String = "Item 1"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        DropTow()
     }
 }
 
---
+++ SwiftUI/Fundamentals/WeatherA/WeatherA/DropdownMenu/PickerMenu.swift
@@ -0,0 +1,18 @@
+//
+//  PickerMenu.swift
+//  WeatherA
+//
+//  Created by Mustafa Bekirov on 27.12.2023.
+//
+
+import SwiftUI
+
+struct PickerMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerMenu()
+}
