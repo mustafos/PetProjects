eca26ef95d1ebee90555f440ada03933654acc8a
Mustafa Bekirov <mustafos1999@gmail.com>
2024-01-02 07:53:43 +0100

    init auto layout project

---
+++ MiniApps/UIKit/Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by Mustafa Bekirov on 01.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
