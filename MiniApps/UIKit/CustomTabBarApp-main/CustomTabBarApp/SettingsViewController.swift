077d02d837b42b84c1b4cfbdeb3363a132fa7758
Mustafa Bekirov <mustafos1999@gmail.com>
2024-01-05 23:10:45 +0100

    feat: custom tab bar

---
+++ MiniApps/UIKit/CustomTabBarApp-main/CustomTabBarApp/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  CustomTabBarApp
+//
+//  Created by Vasichko Anna on 02.06.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+    
+}
---
+++ MiniApps/UIKit/CustomTabBarApp-main/CustomTabBarApp/UIColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExtension.swift
+//  CustomTabBarApp
+//
+//  Created by Vasichko Anna on 04.06.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var tabBarItemAccent: UIColor {
+        #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+    }
+    static var mainWhite: UIColor {
+        #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+    }
+    static var tabBarItemLight: UIColor {
+        #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 0.5084592301)
+    }
+}
