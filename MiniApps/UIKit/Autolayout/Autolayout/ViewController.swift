7ba102a95b3d978dab0416d66320a06188e29f0d
Mustafa Bekirov <mustafos1999@gmail.com>
2024-01-06 20:40:42 +0100

    feat: default margins as spacer guides

--- MiniApps/UIKit/Autolayout/Autolayout/ViewController.swift
+++ MiniApps/UIKit/Autolayout/Autolayout/ViewController.swift
@@ -15,39 +15,17 @@ class ViewController: UIViewController {
     }
     
     func setupViews() -> Void {
-        let topLabel = makeLabel(withText: "top")
-        let bottomLabel = makeLabel(withText: "bottom")
+        let blueView = UIView()
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.backgroundColor = .blue
         
-        let leadingLabel = makeLabel(withText: "leading")
-        let trailingLabel = makeLabel(withText: "trailing")
-        
-        view.addSubview(topLabel)
-        view.addSubview(bottomLabel)
-        view.addSubview(leadingLabel)
-        view.addSubview(trailingLabel)
+        view.addSubview(blueView)
         
         NSLayoutConstraint.activate([
-            topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            bottomLabel.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -10),
-            bottomLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            leadingLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            leadingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            
-            trailingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            trailingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            blueView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            blueView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            blueView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            blueView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
         ])
-    }
-    
-    private func makeLabel(withText text: String) -> UILabel {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = text
-        label.font = UIFont.systemFont(ofSize: 32)
-        label.backgroundColor = .yellow
-        
-        return label
     }
 }
