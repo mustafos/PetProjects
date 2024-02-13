00d0cf9faa57b69333e83c52dc0610b7d0332a03
Mustafa Bekirov <mustafos1999@gmail.com>
2024-02-13 07:39:20 +0100

    feat: hashtag view in line container

--- SwiftUI/Fundamentals/LegoSwiftUI/LegoSwiftUI/LegoSwiftUIApp.swift
+++ SwiftUI/Fundamentals/LegoSwiftUI/LegoSwiftUI/LegoSwiftUIApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct LegoSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HashtagView(tags: hashtags) { tag in
+                print(tag)
+            }
         }
     }
 }
