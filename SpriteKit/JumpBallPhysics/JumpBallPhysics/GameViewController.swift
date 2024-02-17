a34f6b060c8897db0a11034b1765c9db37ecbe39
Mustafa Bekirov <mustafos1999@gmail.com>
2024-02-17 11:52:32 +0100

    feat: jumping physics balls

--- SpriteKit/JumpBallPhysics/JumpBallPhysics/GameViewController.swift
+++ SpriteKit/JumpBallPhysics/JumpBallPhysics/GameViewController.swift
@@ -25,7 +25,7 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
+            view.showsPhysics = true
             view.showsFPS = true
             view.showsNodeCount = true
         }
