4f7c090ce9d63d46d4fbc9da927de03e95fb076f
Mustafa Bekirov <mustafos1999@gmail.com>
2024-02-25 11:56:35 +0100

    feat: physics for ship with random move per second

--- SpriteKit/BasicNodes/BasicNodes/GameScene.swift
+++ SpriteKit/BasicNodes/BasicNodes/GameScene.swift
@@ -10,79 +10,85 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // 1. Create node
+    private var spaceShip: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        scene?.size = UIScreen.main.bounds.size
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // size
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let spaceBackground = SKSpriteNode(imageNamed: "space")
+        spaceBackground.size = CGSize(width: width, height: height)
+        addChild(spaceBackground)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        // 2. init node
+        spaceShip = SKSpriteNode(imageNamed: "spaceShip")
+        spaceShip.xScale = 0.5
+        spaceShip.yScale = 0.5
+        spaceShip.physicsBody = SKPhysicsBody(texture: spaceShip.texture!, size: spaceShip.size)
+        spaceShip.physicsBody?.isDynamic = false
+        addChild(spaceShip)
+        
+        // generation asteroid
+        let asteroidCreate = SKAction.run {
+            let asteroid = self.createAsteroid()
+            self.addChild(asteroid)
         }
+        let asteroidPerSecond: Double = 20
+        let asteroidCreationDelay = SKAction.wait(forDuration: 1.0 / asteroidPerSecond, withRange: 0.5)
+        let asteroidSequenceAction = SKAction.sequence([asteroidCreate, asteroidCreationDelay])
+        let asteroidRunAction = SKAction.repeatForever(asteroidSequenceAction)
+        
+        run(asteroidRunAction)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+    // C = sqrt((x2 - x1)2 + (y2 - y1)2)
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let touch = touches.first {
+            // 3. Determine the point of contact
+            let touchLocation = touch.location(in: self)
+            
+            // 4. Create Action
+            let distance = distanceCalculate(a: spaceShip.position, b: touchLocation)
+            let speed: CGFloat = 500
+            let time = timeToTravelDistance(distance: distance, speed: speed)
+            let moveAction = SKAction.move(to: touchLocation, duration: time)
+            print("time: \(time)")
+            print("distance: \(distance)")
+            
+            spaceShip.run(moveAction)
         }
+    }
+    
+    func distanceCalculate(a: CGPoint, b: CGPoint) -> CGFloat {
+        return sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y))
+    }
+    
+    func timeToTravelDistance(distance: CGFloat, speed: CGFloat) -> TimeInterval {
+        let time = distance / speed
+        return TimeInterval(time)
+    }
+    
+    func createAsteroid() -> SKSpriteNode {
+        let asteroid = SKSpriteNode(imageNamed: "asteroid")
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let randomScale = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: 6)) / 10
+        
+        asteroid.xScale = randomScale
+        asteroid.yScale = randomScale
+        asteroid.position.x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: 6))
+        asteroid.position.y = frame.size.height + asteroid.size.height
+        
+        asteroid.physicsBody = SKPhysicsBody(texture: asteroid.texture!, size: asteroid.size)
+        
+        return asteroid
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+//        let asteroid = createAsteroid()
+//        addChild(asteroid)
     }
 }
