cd2c99c9caf9cd3931c05d849691bdc18b79fca6
Mustafa Bekirov <mustafos1999@gmail.com>
2024-03-07 08:52:46 +0100

    feat: screens for different error messages

---
+++ SwiftUI/Fundamentals/LegoSwiftUI/LegoSwiftUI/TypewriterTextAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  TypewriterTextAnimation.swift
+//  LegoSwiftUI
+//
+//  Created by Mustafa Bekirov on 06.03.2024.
+//
+
+import SwiftUI
+
+struct TypewriterTextAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypewriterTextAnimation()
+}
