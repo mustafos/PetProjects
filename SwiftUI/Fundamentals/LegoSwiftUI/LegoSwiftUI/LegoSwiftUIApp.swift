5162699cb2d66fb3c2f91fe76855af1ec75da3df
Mustafa Bekirov <mustafos1999@gmail.com>
2024-03-09 22:35:12 +0100

    fix: app delegate

--- SwiftUI/Fundamentals/LegoSwiftUI/LegoSwiftUI/LegoSwiftUIApp.swift
+++ SwiftUI/Fundamentals/LegoSwiftUI/LegoSwiftUI/LegoSwiftUIApp.swift
@@ -11,17 +11,7 @@ import SwiftUI
 struct LegoSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-//            LoginView()
-//            LandingPageView()
-//            CaruselContentView()
-//            BlogViewTypeOne()
-//            MapView()
-//            SmoothIconAnimation()
-            TypewriterTextAnimation(text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque pretium lectus id turpis. Nullam faucibus mi quis velit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Mauris metus. Integer tempor.", font: .body)
-//            HashtagView(tags: hashtags) { tag in
-//                print(tag)
-//            }
+            ContentView()
         }
     }
 }
