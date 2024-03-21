b8f11480d2f56d5eab3d98aee05b482812bcfa04
Mustafa Bekirov <mustafos1999@gmail.com>
2024-03-21 08:12:37 +0100

    feat: phpicker with select images id

--- SwiftUI/Fundamentals/LegoSwiftUI/LegoSwiftUI/PHPicker.swift
+++ SwiftUI/Fundamentals/LegoSwiftUI/LegoSwiftUI/PHPicker.swift
@@ -1,18 +1,82 @@
 //
-//  PHPicker.swift
+//  PHPickerView.swift
 //  LegoSwiftUI
 //
-//  Created by Mustafa Bekirov on 17.03.2024.
+//  Created by Mustafa Bekirov on 18.03.2024.
 //
 
 import SwiftUI
+import PhotosUI
 
-struct PHPicker: View {
+var preselectedAssetIdentifiers = [String]()
+
+struct PHPickerView: View {
+    @State private var showPicker: Bool = false
+    
+    var pickerConfig: PHPickerConfiguration {
+        var config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+        config.filter = .images
+        config.selectionLimit = 20
+        config.preferredAssetRepresentationMode = .current
+        config.preselectedAssetIdentifiers = preselectedAssetIdentifiers
+        return config
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("PHPicker") {
+                showPicker.toggle()
+            }
+            .sheet(isPresented: $showPicker, content: {
+                PHPickerSwiftUI(config: pickerConfig) {
+                    selectedImages in
+                    processSelectedImages(results: selectedImages)
+                }
+                .ignoresSafeArea()
+            })
+        }
+        .padding()
+    }
+    
+    func processSelectedImages(results: [PHPickerResult]) {
+        
     }
 }
 
 #Preview {
-    PHPicker()
+    PHPickerView()
+}
+
+struct PHPickerSwiftUI: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    let config: PHPickerConfiguration
+    let completion: (_ selectedImages: [PHPickerResult]) -> Void
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        let controller = PHPickerViewController(configuration: config)
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    class Coordinator: PHPickerViewControllerDelegate {
+        let parent: PHPickerSwiftUI
+        
+        init(parent: PHPickerSwiftUI) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            preselectedAssetIdentifiers = results.map(\.assetIdentifier!)
+            
+            self.parent.completion(results)
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
 }
