12c614f20e652b6750dfeaa1272f8550f053c562
mustafos <mustafos1999@icloud.com>
2024-04-20 19:59:59 +0200

    feat: fetch async await data

--- SwiftUI/Fundamentals/WeatherA/WeatherA/RESTfullAPI/AsyncAwaitView.swift
+++ SwiftUI/Fundamentals/WeatherA/WeatherA/RESTfullAPI/AsyncAwaitView.swift
@@ -5,14 +5,48 @@
 //  Created by Mustafa Bekirov on 17.04.2024.
 //
 
-import SwiftUI
+import Foundation
 
-struct AsyncAwaitView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+private actor ServiceStore {
+    func loadEmployees() async throws -> [Employee] {
+        var employees = [Employee]()
+        let (data, response) = try await URLSession.shared.data(from: Link.employees.url)
+        let httpResponse = response as? HTTPURLResponse
+        let statusCode = httpResponse?.statusCode ?? 0
+        
+        if statusCode == 429 {
+            throw NetworkError.tooManyRequests
+        }
+        
+        guard let decodedQuery = try? JSONDecoder().decode(Query.self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        employees = decodedQuery.data
+        
+        return employees
     }
 }
 
-#Preview {
-    AsyncAwaitView()
+final class AsyncAwaitView: ObservableObject {
+    @Published var employees = [Employee]()
+    @Published var inProgress = false
+    @Published var showError = false
+    @Published var errorMessage = ""
+    
+    private let store = ServiceStore()
+    
+    @MainActor func fetchAllEmployees() async {
+        inProgress = true
+        defer {
+            inProgress = false
+        }
+        do {
+            employees = try await store.loadEmployees()
+        } catch {
+            print("Catch: \(error)")
+            errorMessage = warningMessage(error: error as! NetworkError)
+            showError = true
+        }
+    }
 }
--- SwiftUI/Fundamentals/WeatherA/WeatherA/RESTfullAPI/NetworkGETDataView.swift
+++ SwiftUI/Fundamentals/WeatherA/WeatherA/RESTfullAPI/NetworkGETDataView.swift
@@ -27,7 +27,7 @@ struct NetworkGETDataView: View {
                 
                 Spacer()
             }
-            .navigationTitle("Fetching Data")
+            .navigationTitle("Fetching Async Await")
         }
     }
 }
@@ -37,18 +37,16 @@ struct NetworkGETDataView: View {
 }
 
 struct EmployeesView: View {
-    
-    @StateObject var networkManager = NetworkManager.shared
-    
-    @State private var employees = [Employee]()
-    
-    @State private var showProgress: Bool = false
-    @State private var showError: Bool = false
-    @State private var errorMessage: String = ""
+//    @StateObject var networkManager = NetworkManager.shared
+    @StateObject var networkManager = AsyncAwaitView()
+//    @State private var employees = [Employee]()
+//    @State private var showProgress: Bool = false
+//    @State private var showError: Bool = false
+//    @State private var errorMessage: String = ""
     
     var body: some View {
         ZStack {
-            List(employees, id: \.self) { employee in
+            List(networkManager.employees, id: \.self) { employee in
                 HStack {
                     Text(employee.employee_name)
                     Spacer()
@@ -56,31 +54,55 @@ struct EmployeesView: View {
                 }
             }
             
-            ProgressView()
-                .progressViewStyle(.circular)
-                .opacity(showProgress ? 1 : 0)
-        }
-        .alert(isPresented: $showError, content: {
-            Alert(title: Text(errorMessage))
-        })
-        .onAppear {
-            showProgress = true
-            networkManager.fetchEmployees { result in
-                showProgress = false
-                switch result {
-                case .success(let decodedEmployees):
-                    print("success")
-                    
-                    employees = decodedEmployees
-                    
-                case .failure(let networkError):
-                    print("feilure: \(networkError)")
-                    errorMessage = warningMessage(error: networkError)
-                    showError = true
-                }
+            if networkManager.inProgress {
+                ProgressView()
             }
+//            ProgressView()
+//                .progressViewStyle(.circular)
+//                .opacity(showProgress ? 1 : 0)
+        }
+        .alert(isPresented: $networkManager.showError) {
+            Alert(title: Text(networkManager.errorMessage))
+        }
+//        .onAppear {
+//            showProgress = true
+//            networkManager.fetchEmployees { result in
+//                showProgress = false
+//                switch result {
+//                case .success(let decodedEmployees):
+//                    print("success")
+//                    
+//                    employees = decodedEmployees
+//                    
+//                case .failure(let networkError):
+//                    print("feilure: \(networkError)")
+//                    errorMessage = warningMessage(error: networkError)
+//                    showError = true
+//                }
+//            }
+//        }
+        .task {
+            await networkManager.fetchAllEmployees()
         }
     }
+    
+//    private func fetchData() async {
+//        do {
+//            let (data, response) = try await URLSession.shared.data(from: Link.employees.url)
+//            // check for response status here
+//            if let httpResponse = response as? HTTPURLResponse {
+//                print(httpResponse.statusCode)
+//            }
+//            
+//            // decode that data
+//            if let decodedResponse = try? JSONDecoder().decode(Query.self, from: data) {
+//                print(decodedResponse.status)
+//                employees = decodedResponse.data
+//            }
+//        } catch let connectionError {
+//            print("Wrong data from API: \(connectionError)")
+//        }
+//    }
 }
 
 //________________________________________________________ Model
@@ -117,46 +139,46 @@ enum NetworkError: Error {
     case decodingError
 }
 
-final class NetworkManager: ObservableObject {
-    
-    init() {}
-    
-    static let shared = NetworkManager()
-    
-    func fetchEmployees(completion: @escaping (Result<[Employee], NetworkError>) -> Void) {
-        print("try to fetch")
-        
-        let fetchRequest = URLRequest(url: Link.employees.url)
-        
-        URLSession.shared.dataTask(with: fetchRequest) { (data, response, error) -> Void in
-            if error != nil {
-                print("Error in session is not nil")
-                completion(.failure(.noData))
-            } else {
-                // We've got data!
-                let httpResponse = response as? HTTPURLResponse
-                print("status code: \(String(describing: httpResponse?.statusCode ?? 0))")
-                
-                if httpResponse?.statusCode == 429 {
-                    completion(.failure(.tooManyRequests))
-                } else {
-                    guard let safeData = data else { return }
-                    
-                    do {
-                        let decodedQuery = try JSONDecoder().decode(Query.self, from: safeData)
-                        
-                        completion(.success(decodedQuery.data))
-                        
-                    } catch let decodeError {
-                        print("Decoding error: \(decodeError)")
-                        completion(.failure(.decodingError))
-                    }
-                }
-            }
-        }
-        .resume()
-    }
-}
+//final class NetworkManager: ObservableObject {
+//    
+//    init() {}
+//    
+//    static let shared = NetworkManager()
+//    
+//    func fetchEmployees(completion: @escaping (Result<[Employee], NetworkError>) -> Void) {
+//        print("try to fetch")
+//        
+//        let fetchRequest = URLRequest(url: Link.employees.url)
+//        
+//        URLSession.shared.dataTask(with: fetchRequest) { (data, response, error) -> Void in
+//            if error != nil {
+//                print("Error in session is not nil")
+//                completion(.failure(.noData))
+//            } else {
+//                // We've got data!
+//                let httpResponse = response as? HTTPURLResponse
+//                print("status code: \(String(describing: httpResponse?.statusCode ?? 0))")
+//                
+//                if httpResponse?.statusCode == 429 {
+//                    completion(.failure(.tooManyRequests))
+//                } else {
+//                    guard let safeData = data else { return }
+//                    
+//                    do {
+//                        let decodedQuery = try JSONDecoder().decode(Query.self, from: safeData)
+//                        
+//                        completion(.success(decodedQuery.data))
+//                        
+//                    } catch let decodeError {
+//                        print("Decoding error: \(decodeError)")
+//                        completion(.failure(.decodingError))
+//                    }
+//                }
+//            }
+//        }
+//        .resume()
+//    }
+//}
 
 //________________________________________________________ Helper
 func warningMessage(error: NetworkError) -> String {
