25ed3716fa9e192e0413e5d2291724d295990b5c
mustafos <mustafos1999@icloud.com>
2024-07-11 12:00:00 +0200

    init Crypto App project

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Mustafa Bekirov on 11.07.2024.
+//  Copyright © 2024 Mustafa Bekirov. All rights reserved.
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoApp/CryptoApp/Extension/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Mustafa Bekirov on 11.07.2024.
+//  Copyright © 2024 Mustafa Bekirov. All rights reserved.
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color.green
+    let red = Color.red
+    let secondaryText = Color.gray
+}
