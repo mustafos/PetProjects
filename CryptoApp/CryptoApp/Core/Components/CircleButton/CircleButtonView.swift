578cd687e1151b4453d088552e5d6d5ffa7505d6
mustafos <mustafos1999@icloud.com>
2024-07-14 13:05:06 +0200

    feat: row design to display coins in a list

--- CryptoApp/CryptoApp/Core/Components/CircleButton/CircleButtonView.swift
+++ CryptoApp/CryptoApp/Core/Components/CircleButton/CircleButtonView.swift
@@ -20,8 +20,9 @@ struct CircleButtonView: View {
                 Circle()
                     .foregroundColor(Color.theme.background)
             )
-            .shadow(color: Color.theme.accent.opacity(0.25),
-                    radius: 10, x: 0.0, y: 0.0)
+            .shadow(
+                color: Color.theme.accent.opacity(0.25),
+                radius: 10, x: 0, y: 0)
             .padding()
     }
 }
