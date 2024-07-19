42555fa1cc4a03d020ef411f06dfbf6a7b7327be
mustafos <mustafos1999@icloud.com>
2024-07-19 18:32:25 +0200

    feat: adding a networking layer

--- CryptoApp/CryptoApp/Services/CoinDataService.swift
+++ CryptoApp/CryptoApp/Services/CoinDataService.swift
@@ -11,6 +11,7 @@ import Combine
 class CoinDataService {
     
     @Published var allCoins: [CoinModel] = []
+    
     var coinSubscription: AnyCancellable?
     
     init() {
@@ -20,26 +21,12 @@ class CoinDataService {
     private func getCoins() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
-            }
+            })
     }
 }
---
+++ CryptoApp/CryptoApp/Utilities/NetworkingManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkingManager.swift
+//  CryptoApp
+//
+//  Created by Mustafa Bekirov on 19.07.2024.
+//  Copyright © 2024 Mustafa Bekirov. All rights reserved.
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🔥] Bad response from URL: \(url)"
+            case .unknown: return "[⚠️] Unknown error occured"
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .retry(3)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
