efe6783268395f54d0aa31c29fbb0939c478a2a9
mustafos <mustafos1999@icloud.com>
2024-11-03 06:15:23 +0100

    feat: IAP service for in-app purchase

---
+++ StoreKit/Foodzilla/Services/IAPService.swift
@@ -0,0 +1,150 @@
+//
+//  IAPService.swift
+//  Foodzilla
+//
+//  Created by Caleb Stultz on 9/14/17.
+//  Copyright © 2017 Caleb Stultz. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+protocol IAPServiceDelegate {
+    func iapProductsLoaded()
+}
+
+class IAPService: NSObject, SKProductsRequestDelegate {
+    static let instance = IAPService()
+    
+    var delegate: IAPServiceDelegate?
+    
+    var products = [SKProduct]()
+    var productIds = Set<String>()
+    var productRequest = SKProductsRequest()
+    
+    var nonConsumablePurchaseWasMade = UserDefaults.standard.bool(forKey: "nonConsumablePurchaseWasMade")
+    
+    override init() {
+        super.init()
+        SKPaymentQueue.default().add(self)
+    }
+    
+    func loadProducts() {
+        productIdToStringSet()
+        requestProducts(forIds: productIds)
+    }
+    
+    func productIdToStringSet() {
+        let ids = [IAP_HIDE_ADS_ID, IAP_MEAL_ID]
+        for id in ids {
+            productIds.insert(id)
+        }
+    }
+    
+    func requestProducts(forIds ids: Set<String>) {
+        productRequest.cancel()
+        productRequest = SKProductsRequest(productIdentifiers: ids)
+        productRequest.delegate = self
+        productRequest.start()
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        self.products = response.products
+        
+        if products.count == 0 {
+            requestProducts(forIds: productIds)
+        } else {
+            delegate?.iapProductsLoaded()
+        }
+    }
+    
+    func attemptPurchaseForItemWith(productIndex: Product) {
+        let product = products[productIndex.rawValue]
+        let payment = SKPayment(product: product)
+        SKPaymentQueue.default().add(payment)
+    }
+    
+    func restorePurchases() {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+}
+
+extension IAPService: SKPaymentTransactionObserver {
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchased:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                complete(transaction: transaction)
+                sendNotificationFor(status: .purchased, withIdentifier: transaction.payment.productIdentifier)
+                debugPrint("Purchase was successful!")
+                break
+            case .restored:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                break
+            case .failed:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                sendNotificationFor(status: .failed, withIdentifier: nil)
+                break
+            case .deferred:
+                break
+            case .purchasing:
+                break
+            }
+        }
+    }
+    
+    func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+        sendNotificationFor(status: .restored, withIdentifier: nil)
+        setNonConsumablePurchase(true)
+    }
+    
+    func complete(transaction: SKPaymentTransaction) {
+        switch transaction.payment.productIdentifier {
+        case IAP_MEAL_ID:
+            break
+        case IAP_HIDE_ADS_ID:
+            setNonConsumablePurchase(true)
+            break
+        default:
+            break
+        }
+    }
+    
+    func setNonConsumablePurchase(_ status: Bool) {
+        UserDefaults.standard.set(status, forKey: "nonConsumablePurchaseWasMade")
+    }
+    
+    func sendNotificationFor(status: PurchaseStatus, withIdentifier identifier: String?) {
+        switch status {
+        case .purchased:
+            NotificationCenter.default.post(name: NSNotification.Name(IAPServicePurchaseNotification), object: identifier)
+            break
+        case .restored:
+            NotificationCenter.default.post(name: NSNotification.Name(IAPServiceRestoreNotification), object: nil)
+            break
+        case .failed:
+            NotificationCenter.default.post(name: NSNotification.Name(IAPServiceFailureNotification), object: nil)
+            break
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
