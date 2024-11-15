73b469f4a85d395f221bd02c878bc75be7e30c78
mustafos <mustafos1999@icloud.com>
2024-11-15 22:59:34 +0100

    feat: product and purchase status

---
+++ StoreKit/Foodzilla/Enums/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  Foodzilla
+//
+//  Created by Caleb Stultz on 9/14/17.
+//  Copyright © 2017 Caleb Stultz. All rights reserved.
+//
+
+import Foundation
+
+enum Product: Int {
+    case hideAds = 0
+    case meal = 1
+}
---
+++ StoreKit/Foodzilla/Enums/PurchaseStatus.swift
@@ -0,0 +1,15 @@
+//
+//  PurchaseStatus.swift
+//  Foodzilla
+//
+//  Created by Caleb Stultz on 9/14/17.
+//  Copyright © 2017 Caleb Stultz. All rights reserved.
+//
+
+import Foundation
+
+enum PurchaseStatus {
+    case purchased
+    case restored
+    case failed
+}
