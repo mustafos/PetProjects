c85a0f5faeedd720bff56ac202d8b666a53cbaa6
mustafos <mustafos1999@icloud.com>
2024-12-05 16:31:22 +0100

    init: TCA architecture template

---
+++ Architecture/ReduxApp/ReduxApp/ReduxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxAppApp.swift
+//  ReduxApp
+//
+//  Created by Mustafa Bekirov on 03.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReduxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Architecture/ReduxApp/ReduxApp/Store/Store.swift
@@ -0,0 +1,8 @@
+//
+//  Store.swift
+//  ReduxApp
+//
+//  Created by Mustafa Bekirov on 03.01.2025.
+//
+
+import Foundation
