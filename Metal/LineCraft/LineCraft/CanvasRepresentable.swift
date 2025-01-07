f37a13d6e728ffc7b864f582861b1fe92d39a09d
mustafos <mustafos1999@icloud.com>
2025-01-07 12:27:14 +0100

    fix: clean methods

--- Metal/LineCraft/LineCraft/CanvasRepresentable.swift
+++ Metal/LineCraft/LineCraft/CanvasRepresentable.swift
@@ -6,28 +6,28 @@
 //
 
 import SwiftUI
-
-struct CanvasRepresentable: UIViewRepresentable {
-    // Create an instance of your custom DrawingCanvasView
-    private var drawingCanvasView = DrawingCanvasView()
-    
-    // Method to create and configure the view
-    func makeUIView(context: Context) -> DrawingCanvasView {
-        return drawingCanvasView
-    }
-    
-    // Method to update the view
-    func updateUIView(_ uiView: DrawingCanvasView, context: Context) {
-        // You can update any dynamic properties here if needed
-    }
-    
-    // Метод для додавання лінії за координатами
-    func addLine(from start: CGPoint, to end: CGPoint) {
-        drawingCanvasView.addLine(from: start, to: end)
-    }
-    
-    // Method to clear the canvas
-    func clearCanvas() {
-        drawingCanvasView.clearCanvas() // Call the clearCanvas() method on your custom view
-    }
-}
+//
+//struct CanvasRepresentable: UIViewRepresentable {
+//    // Create an instance of your custom DrawingCanvasView
+//    private var drawingCanvasView = DrawingCanvasView()
+//    
+//    // Method to create and configure the view
+//    func makeUIView(context: Context) -> DrawingCanvasView {
+//        return drawingCanvasView
+//    }
+//    
+//    // Method to update the view
+//    func updateUIView(_ uiView: DrawingCanvasView, context: Context) {
+//        // Trigger canvas redraw here if needed
+//    }
+//    
+//    // Метод для додавання лінії за координатами
+//    func addLine(from start: CGPoint, to end: CGPoint) {
+//        drawingCanvasView.addLine(from: start, to: end)
+//    }
+//    
+//    // Method to clear the canvas
+//    func clearCanvas() {
+//        drawingCanvasView.clearCanvas() // Call the clearCanvas() method on your custom view
+//    }
+//}
