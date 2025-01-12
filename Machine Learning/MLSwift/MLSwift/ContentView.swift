7332b9a67b0ff28228e6605c1059a7c3efd1596c
mustafos <mustafos1999@icloud.com>
2025-01-12 18:21:23 +0100

    init MLSwift project

---
+++ Machine Learning/MLSwift/MLSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MLSwift
+//
+//  Created by Mustafa Bekirov on 07.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Machine Learning/MLSwift/MLSwift/MLSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLSwiftApp.swift
+//  MLSwift
+//
+//  Created by Mustafa Bekirov on 07.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MLSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
