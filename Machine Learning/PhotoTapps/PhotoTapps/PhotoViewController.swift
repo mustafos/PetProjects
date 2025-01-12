9b97170dbe8a639ebcd3af73103dd88c3df05a0d
mustafos <mustafos1999@icloud.com>
2025-01-12 09:34:56 +0100

    feat: photo view controller with data model and picker delegate

--- Machine Learning/PhotoTapps/PhotoTapps/PhotoViewController.swift
+++ Machine Learning/PhotoTapps/PhotoTapps/PhotoViewController.swift
@@ -2,36 +2,98 @@
 //  PhotoViewController.swift
 //  PhotoTapps
 //
-//  Created by Mustafa Bekirov on 09.01.2025.
+//  Created by Mustafa Bekirov on 11.01.2025.
 //
 
+import CoreML
 import UIKit
 
-class PhotoViewController: UIViewController {
-    
-    var image: UIImage?
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var photoImageView: UIImageView!
-    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "photo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Select Image"
+        label.numberOfLines = 0
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        photoImageView.image = image
-    }
-    
-    @IBAction func shareAction(_ sender: Any) {
-        
-        let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
-        
-        shareController.completionWithItemsHandler = { _, bool, _, _ in
-            if bool {
-                print("Success")
-            }
-        }
-        
-        present(shareController, animated: true)
-        
-    }
-    
+        view.addSubview(label)
+        view.addSubview(imageView)
 
+        let tap = UITapGestureRecognizer(
+            target: self,
+            action: #selector(didTapImage)
+        )
+        tap.numberOfTapsRequired = 1
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tap)
+    }
+
+    @objc func didTapImage() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.frame = CGRect(
+            x: 20,
+            y: view.safeAreaInsets.top,
+            width: view.frame.size.width-40,
+            height: view.frame.size.width-40)
+        label.frame = CGRect(
+            x: 20,
+            y: view.safeAreaInsets.top+(view.frame.size.width-40)+10,
+            width: view.frame.size.width-40,
+            height: 100
+        )
+    }
+
+    private func analyzeImage(image: UIImage?) {
+        guard let buffer = image?.resize(size: CGSize(width: 224, height: 224))?
+                .getCVPixelBuffer() else {
+            return
+        }
+
+        do {
+            let config = MLModelConfiguration()
+            let model = try GoogLeNetPlaces(configuration: config)
+            let input = GoogLeNetPlacesInput(sceneImage: buffer)
+
+            let output = try model.prediction(input: input)
+            let text = output.sceneLabel
+            label.text = text
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    // Image Picker
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // cancelled
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        imageView.image = image
+        analyzeImage(image: image)
+    }
 }
