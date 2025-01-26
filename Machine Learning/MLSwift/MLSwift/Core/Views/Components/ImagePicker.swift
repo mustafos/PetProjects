b2571876a6d267fd14df7fa5c32b57b623f7bb6a
mustafos <mustafos1999@icloud.com>
2025-01-26 17:49:31 +0100

    feat: image picker with representable controller

---
+++ Machine Learning/MLSwift/MLSwift/Core/Views/Components/ImagePicker.swift
@@ -0,0 +1,45 @@
+//
+//  ImagePicker.swift
+//  MLSwift
+//
+//  Created by Mustafa Bekirov on 26.01.2025.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var selectedImage: UIImage?
+    @Binding var imageName: String?
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = uiImage
+                parent.imageName = "IMG\(UUID().uuidString.prefix(4))"
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
