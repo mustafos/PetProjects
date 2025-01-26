bc4f1a0db7ae82e60f53f45afef29bdd85384901
mustafos <mustafos1999@icloud.com>
2025-01-26 20:34:45 +0100

    feat: add image picker and traffic light detection

--- Machine Learning/MLSwift/MLSwift/Core/Views/ObjectDetectionView.swift
+++ Machine Learning/MLSwift/MLSwift/Core/Views/ObjectDetectionView.swift
@@ -9,11 +9,14 @@ import Vision
 import SwiftUI
 
 struct ObjectDetectionView: View {
-    @State private var image: UIImage? = UIImage(named: "sample")
+    @State private var image: UIImage? = nil
+    @State private var imageName: String?
     @State private var detections: [VNRecognizedObjectObservation] = []
-
+    @State private var showImagePicker = false
+    @State private var isLoading = false
+    
     let yoloModel = YOLOv3Model()
-
+    
     var body: some View {
         VStack {
             if let image = image {
@@ -27,17 +30,57 @@ struct ObjectDetectionView: View {
                             BoundingBoxView(boundingBox: detection.boundingBox, imageSize: image.size, viewSize: geometry.size)
                         }
                     }
-                }
+                }.frame(height: 400)
+            } else {
+                Text("Select an image to detect traffic lights")
+                    .font(.title)
+                    .foregroundColor(.gray)
+                    .frame(height: 400)
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        withAnimation {
+                            isLoading = true
+                            showImagePicker = true
+                        }
+                    }
             }
             
-            Button("Detect Objects") {
-                if let img = image {
-                    yoloModel.detectObjects(in: img) { results in
-                        self.detections = results
+            HStack {
+                Button("Choose Image") {
+                    isLoading = true
+                    showImagePicker = true
+                    if image != nil {
+                        self.image = nil
+                        self.imageName = nil
+                        self.detections = []
+                        isLoading = false
                     }
                 }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                
+                Button("Detect Traffic Lights") {
+                    if let object = image {
+                        yoloModel.detectObjects(in: object) { results in
+                            self.detections = results
+                        }
+                    }
+                }
+                .padding()
+                .background(image == nil ? Color.gray : Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .disabled(image == nil)
             }
             .padding()
+        }
+        .sheet(isPresented: $showImagePicker) {
+            isLoading.toggle()
+        } content: {
+            ImagePicker(selectedImage: self.$image, imageName: self.$imageName).ignoresSafeArea()
         }
     }
 }
