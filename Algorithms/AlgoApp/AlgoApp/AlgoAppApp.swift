44e575c8a53854a1a02bde5f6002be39ec8e420e
mustafos <mustafos1999@icloud.com>
2025-01-30 18:34:20 +0100

    init AlgoApp

---
+++ Algorithms/AlgoApp/AlgoApp/AlgoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoAppApp.swift
+//  AlgoApp
+//
+//  Created by Mustafa Bekirov on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AlgoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
