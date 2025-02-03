2bbe9ebd1c0d8a57e7509ef1d111cc8a5fa13058
mustafos <mustafos1999@icloud.com>
2025-02-03 05:11:18 +0100

    feat: environment sorting settings value

---
+++ Algorithms/AlgoApp/AlgoApp/Settings/EnvironmentValues.swift
@@ -0,0 +1,12 @@
+//
+//  EnvironmentValues.swift
+//  AlgoApp
+//
+//  Created by Mustafa Bekirov on 03.02.2025.
+//
+
+import SwiftUI
+
+extension EnvironmentValues {
+    @Entry var sortingSettings = SortingSettings.shared
+}
